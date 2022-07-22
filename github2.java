4ebec44799846d7f53ef33fc67820b9fbe5b751c
Sonal Patel <tina_sonal@yahoo.co.in>
2022-07-22 19:49:54 +0100

    few changes

---
+++ github2.java
@@ -0,0 +1,11 @@
+public class github2 {
+
+    public static void main(String[] args) {
+        int a=100;
+                int b=200;
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
